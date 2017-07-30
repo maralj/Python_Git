02020dcf3af702513b987ea072139214052a0020
Maral Jam <maral.jam@icloud.com>
2017-07-30 14:15:34 -0700

    Python trutle

---
+++ artzi.py
@@ -0,0 +1,41 @@
+import turtle
+import math
+pencil = turtle.Turtle()
+def art(radius):
+    c = 2*math.pi*radius
+    side = int(c/10)+1
+    length = c/side
+    rotate = 360/90
+    number = 90
+    # setting thre defult color to blue
+    color = "red"
+    for k in range (number):
+        print(color)
+        #if k < number/2:
+         #   color = "blue"
+         #   print("blue ")
+        #else:
+    # changing the color for the 2nd half
+         #   color = "red"
+          #  print("red")
+        for i in range (side):
+            pencil.pencolor(color)
+            pencil.fd(length)
+            pencil.lt(360/side)
+        pencil.lt(rotate)
+        if color == "red":
+            color = "purple"
+        elif color == "purple":
+            color = "blue"
+        elif color == "blue":
+            color = "green"
+        elif color == "green":
+            color = "yellow"
+        elif color == "yellow":
+            color = "orange"
+        elif color == "orange":
+            color = "brown"
+        elif color == "brown":
+            color = "red"
+            
+art(100)
